a8577eed85c20af1784389166899c802859d8955
Tyler Downing <tyler.ar.downing@hotmail.com>
2024-08-19 15:08:44 -0300

    finishd problem 7

---
+++ PrjectEuler7.py
@@ -0,0 +1,19 @@
+#what is the 10001 prime number?
+from sympy import *
+
+def find_num_of_prime(x):
+    list_of_primes = []
+    i = 1
+    while len(list_of_primes) < x:
+        if isprime(i) == True:
+            list_of_primes.append(i)
+            i+=1
+        else:
+            i+=1
+    print(list_of_primes[x-1])
+    
+
+
+find_num_of_prime(10001)
+
+
